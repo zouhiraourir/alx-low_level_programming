0c5306dd85831b95522f52cc0bbbb7195c49f02b
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-13 16:24:06 -0700

    d

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description.: c program that prints puts function 
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    puts("\"Programming is like buiding a multilingual puzzle");
+    return (0);
+}
