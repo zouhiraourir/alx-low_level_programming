f412f9d2a96308dc04f9fe0d0f98e83d7edf267b
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-13 15:58:56 -0700

    f

---
+++ 5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * return: Always 0 (Success
+ */
+
+int main(void)
+{
+    puts("with proper grammar, but the outcome is piece of art,\n");
+    return (0);
+}
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                                                                                      
+~                                                             
