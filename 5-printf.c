134bf547f25dba816d82e17b91fe9656ec7cf0bd
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-13 16:21:56 -0700

    g

--- 5-printf.c
+++ 5-printf.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- * return: Always 0 (Success
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    printf("with proper grammar, but the outcome is piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 ~                                                                                                                      
 ~                                                                                                                      
