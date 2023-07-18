bd1aca6ca6585d4872f42acb235bb9f4b248c643
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-18 16:16:15 -0700

    b

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
+  * Return:Always 0 (success)
+  */
+void print_alphabet_x10(void)
+{
+        int j;
+
+                for (j = 'a'; j <= 'z'; j++)
+                {
+                        _putchar(j);
+                }
+                _putchar('\n');
+}
