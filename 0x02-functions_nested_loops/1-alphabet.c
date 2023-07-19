9d3343111882c2610db34484f8e3796b6e3b650e
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-19 07:53:19 -0700

    b

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_alphabet - prints the alphabet
+  *
+  * Return: Always 0 (success)
+  */
+void print_alphabet(void)
+{
+        int e;
+
+        for (e = 'a'; e <= 'z'; e++)
+        {
+                _putchar(e);
+        }
+
+        _putchar('\n');
+}
