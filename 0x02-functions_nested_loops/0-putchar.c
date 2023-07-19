3a2f0513bb8464c1ff993982a34552ed80de4198
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-19 08:07:16 -0700

    b

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-        char str[] = "_putchar";
-        int ch;
+	char str[] = "_putchar";
+	int ch;
 
-        for (ch = 0; ch < 8; ch++)
-                _putchar(str[ch]);
-        _putchar('\n');
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
 
-        return (0);
+	return (0);
 }
