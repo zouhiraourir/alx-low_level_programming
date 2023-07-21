5c716bec9fd4c3648f282f7e08a15c8a6ae1b908
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-21 15:35:59 -0700

    l

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - printing lines
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		putchar('');
+	}
+	_putchar('\n');
+}
