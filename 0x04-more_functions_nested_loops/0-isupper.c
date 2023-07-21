dd900860a4e9a6e6e325c0194e2c4ed66f54c9ff
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-21 14:32:58 -0700

    d

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,10 @@
 
 int -isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{	
 		return (1);
-	}
 	else
-	{
 		return (0);
 	}
 }      
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-
 int _isupper(int c);
 int putchar(int char);
 int _isdigit(int c);
@@ -14,5 +9,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
-#endif
