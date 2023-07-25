fdf4b66d641171431a71df7f8fdfc8ea67f596eb
zouhiraourir <zouhiraourir4@gmail.com>
2023-07-25 08:36:42 -0700

    f

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+	void reset_to_98(int *n)
+	*n=98
+
+int main (void){
+
+	int n;
+	int *n ;
+	*n=&n;
+	
+		printf(n=%d\n,n);
+	
+	return(0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+pointers strings and arrays 
+
